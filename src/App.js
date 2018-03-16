56fe794f9ca8bdb258381171c688e8cf913aa02a
Matthew Wilson <matthew@productearthsolutions.com>
2018-03-16 00:01:00 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <div class="row align-items-center row-full-height">
+          <div class="col">
+            <img className="img-fluid mx-auto d-block" src="/img/borith.png"/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
